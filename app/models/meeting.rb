54207400e7ec0545a510bc89150cba90361b867c
mi-ma-ri <michihiros93@gmail.com>
2022-04-23 13:22:35 +0900

    モデル修正

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,8 +1,10 @@
 class Meeting < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :content, presence: true
   validates :attack,  presence: true
   validates :defence, presence: true
+  validates :attack_url, presence: true
+  validates :defence_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
     has_many :meetings
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
-    validates :name, presence: true
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
+    
+    validates :name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, massage: "名前を入れてください" }
     validates :team_name, presence: true
-    validates :number, presence: true
+    validates :number, presence: true, format: { with: /\A[0-9]+\z/, massage: "背番号を入れてください" }
 end
